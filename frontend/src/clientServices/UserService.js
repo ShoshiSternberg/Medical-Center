db0b0b4eefdc13f291fb8738784f407684cb842d
medical-center-elad <medicalcenterelad@gmail.com>
2024-09-03 18:54:23 +0300

    Update UserService.js

--- frontend/src/clientServices/UserService.js
+++ frontend/src/clientServices/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/users';
+const API_URL = 'https://medical-center-znya.onrender.com/users';
 
 export const getUsers = async () => {
   try {
