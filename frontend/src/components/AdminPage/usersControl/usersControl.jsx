25899251c57f10b32a5ac0cdf95a6a5918d3b73a
medical-center-elad <medicalcenterelad@gmail.com>
2024-09-12 21:58:19 +0300

    WIP: Save changes before switching branches

--- frontend/src/components/AdminPage/usersControl/usersControl.jsx
+++ frontend/src/components/AdminPage/usersControl/usersControl.jsx
@@ -33,6 +33,13 @@ function MyVerticallyCenteredModal(props) {
     });
 
     const [roles, setRoles] = useState([]);
+    const [errors, setErrors] = useState({
+        Name: '',
+        RoleID: '',
+        Password: '',
+        Email: '',
+        Phone: ''
+    });
 
     const fetchRoles = async () => {
         try {
@@ -48,34 +55,64 @@ function MyVerticallyCenteredModal(props) {
     }, []);
 
     const validateUserName = () => {
-        if (!newUser.Name) return false;
-
+        if (!newUser.Name) {
+            setErrors(prevErrors => ({ ...prevErrors, Name: 'שם משתמש נדרש' }));
+            console.log(errors);
+            return false;
+        }
+        setErrors(prevErrors => ({ ...prevErrors, Name: '' }));
+        return true;
     };
 
     const validatePhoneNumber = () => {
-        if (!newUser.Phone) return false;
+        if (!newUser.Phone) {
+            setErrors(prevErrors => ({ ...prevErrors, Phone: 'מספר טלפון נדרש' }));
+            return false;
+        }
         const regex = /^\d{10}$/;
-        console.log(regex.test(newUser.Phone) + 'טלפון')
-        return regex.test(newUser.Phone);
-
+        if (!regex.test(newUser.Phone)) {
+            setErrors(prevErrors => ({ ...prevErrors, Phone: 'מספר טלפון לא תקין' }));
+            return false;
+        }
+        setErrors(prevErrors => ({ ...prevErrors, Phone: '' }));
+        return true;
     };
 
     const validatePassword = () => {
-        if (!newUser.Password) return false;
+        if (!newUser.Password) {
+            setErrors(prevErrors => ({ ...prevErrors, Password: 'סיסמה נדרשת' }));
+            return false;
+        }
         const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*]{6,}$/;
-        console.log(regex.test(newUser.Password) + 'סיסמא')
-        return regex.test(newUser.Password);
+        if (!regex.test(newUser.Password)) {
+            setErrors(prevErrors => ({ ...prevErrors, Password: 'סיסמה חייבת להכיל לפחות 6 תווים, כולל אות ומספר' }));
+            return false;
+        }
+        setErrors(prevErrors => ({ ...prevErrors, Password: '' }));
+        return true;
     };
 
     const validateEmail = () => {
-        if (!newUser.Email) return false;
+        if (!newUser.Email) {
+            setErrors(prevErrors => ({ ...prevErrors, Email: 'כתובת אימייל נדרשת' }));
+            return false;
+        }
         const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        console.log(regex.test(newUser.Email) + 'מייל')
-        return regex.test(newUser.Email);
+        if (!regex.test(newUser.Email)) {
+            setErrors(prevErrors => ({ ...prevErrors, Email: 'כתובת אימייל לא תקינה' }));
+            return false;
+        }
+        setErrors(prevErrors => ({ ...prevErrors, Email: '' }));
+        return true;
     };
 
     const validateRole = () => {
-        return newUser.RoleID !== 0;
+        if (newUser.RoleID === '' || newUser.RoleID === 0) {
+            setErrors(prevErrors => ({ ...prevErrors, RoleID: 'תפקיד נדרש' }));
+            return false;
+        }
+        setErrors(prevErrors => ({ ...prevErrors, RoleID: '' }));
+        return true;
     };
 
     const validateInputs = () => {
@@ -85,7 +122,7 @@ function MyVerticallyCenteredModal(props) {
     const handleCreateUser = async () => {
         try {
             if (!validateInputs()) {
-                alert("נתונים לא תקינים");
+                //alert("נתונים לא תקינים");
                 return;
             }
             const isExist = await getUserByEmailAddress(newUser.Email);
@@ -121,6 +158,8 @@ function MyVerticallyCenteredModal(props) {
                         label="שם"
                         value={newUser.Name}
                         onChange={(e) => setNewUser({ ...newUser, Name: e.target.value })}
+                        error={errors.Name!=''}
+                        helperText={errors.Name}
                         fullWidth
                         margin="normal"
                     />
@@ -129,6 +168,8 @@ function MyVerticallyCenteredModal(props) {
                         label="תפקיד"
                         value={newUser.RoleID}
                         onChange={(e) => setNewUser({ ...newUser, RoleID: e.target.value })}
+                        error={!!errors.RoleID}
+                        helperText={errors.RoleID}
                         fullWidth
                         margin="normal"
                     >
@@ -144,6 +185,8 @@ function MyVerticallyCenteredModal(props) {
                         label="סיסמה"
                         value={newUser.Password}
                         onChange={(e) => setNewUser({ ...newUser, Password: e.target.value })}
+                        error={!!errors.Password}
+                        helperText={errors.Password}
                         fullWidth
                         margin="normal"
                     />
@@ -152,6 +195,8 @@ function MyVerticallyCenteredModal(props) {
                         label="אימייל"
                         value={newUser.Email}
                         onChange={(e) => setNewUser({ ...newUser, Email: e.target.value })}
+                        error={!!errors.Email}
+                        helperText={errors.Email}
                         fullWidth
                         margin="normal"
                     />
@@ -159,6 +204,8 @@ function MyVerticallyCenteredModal(props) {
                         label="טלפון"
                         value={newUser.Phone}
                         onChange={(e) => setNewUser({ ...newUser, Phone: e.target.value })}
+                        error={!!errors.Phone}
+                        helperText={errors.Phone}
                         fullWidth
                         margin="normal"
                     />
